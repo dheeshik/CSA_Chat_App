9cf2118eacdded6091991ce25198fdadb3541714
Dheeshik Ramachandran <dheeshik@gmail.com>
2023-10-19 12:47:31 -0700

    main.java

--- DS Chat/src/Main.java
+++ DS Chat/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        GUI window = new GUI();
-
+  public static void main(String[] args) {
+       GUI window = new GUI();
 
 
     }
